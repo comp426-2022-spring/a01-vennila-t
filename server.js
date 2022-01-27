806d2ae78888858c2f8a2555331536d63154d81f
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-01-27 12:57:38 -0500

    server.js

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ if (err) {
 }
 
 
-
-
 // Define a const `server` as an arrow function using http.createServer.
 // Use the documentation for the node.js http module.
 // The function should have three responses:
@@ -39,16 +37,11 @@ const server = https.createServer((reg, res) => {
 })
 
 
-
-
-
-
 // Start the `server` const listening on the port defined by argument in your `port` const.
 // Put the exact message `Server listening on port ${port}` on the console log.
 server.listen(port, () => {
     console.log('Server running at port ${port}')
 })
-
 
 
 // That's it! You're all done!
