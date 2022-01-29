5cbdbfe1420f7782006ee1cf3e9835275da8ef39
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-01-28 19:14:42 -0500

    server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (err) {
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello, World!</h1>')
+    res.end('<td>Z</td><td></td>')
 })
 
 
